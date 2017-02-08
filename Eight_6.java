a622783f6a1a9571420c768f236257310bda870a
fstonezst <fstone.zh@gmail.com>
2017-02-08 10:52:27 +0800

    Update Eight_6.java
    添加说明

--- Eight_6.java
+++ Eight_6.java
@@ -3,23 +3,22 @@ public class Eight_6{
 	/*切割木棍问题*/
 	public static void main(String[] args){
         int[] p = new int[]{1,3,4,5,8,9,10};
-        System.out.println(maxPrice(p));
+             System.out.println(maxPrice(p));
 	}
-	public static int maxPrice(int[] p )
-    {
-        int[] maxPrice = new int[p.length+1];
-        int[] trace = new int[maxPrice.length];
+	public static int maxPrice(int[] p)
+        {
+             int[] maxPrice = new int[p.length+1];
+             int[] trace = new int[maxPrice.length];
         for (int k = 1;k <= p.length; ++k)
         {
             maxPrice[k] = p[k-1];
         }
-        for (int i = 1; i < maxPrice.length;++i)
+        for (int i = 1; i < maxPrice.length;++i)//从1开始计算长度为i的木棍所能得到的最大价格
         {
             trace[i] = 0;
-            for (int j = 1 ; j < i;++j)
+            for (int j = 1 ; j < i;++j)//长度为i的木i棍最大价格划分的可能情况为 price（j） j： i 到i-1 与 max（i-j）之和。
             {
-                int price = maxPrice[j] + maxPrice[i-j] ;
-                if ( price > maxPrice[i]) {
+                    int price = maxPrice[j] + maxPrice[i-j];
                     maxPrice[i] = price;
                     trace[i] = j;
                 }
